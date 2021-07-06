544f847fa2d3f7e5269e6d82ce53a0cdae4e9aef
TaarygooSan <69000252+TaarygooSan@users.noreply.github.com>
2021-07-06 16:46:32 +0800

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 """
-作者：王振东
+作者：TaarygooSan
 日期：2021年07月03日
 
  常用快捷键
@@ -31,21 +31,45 @@
 import pygame, sys  # 引用库
 
 pygame.init()  # 初始化，必须
-size = width, height = 600, 600  # 设置窗口大小参数
-screen = pygame.display.set_mode(size)  # 生成窗口
+vInfo = pygame.display.Info()  # 获得当 前窗口信息
+size = width, height = 400, 600
+# size = width, height = vInfo.current_w, vInfo.current_h  # 设置窗口大小参数
+# screen = pygame.display.set_mode(size)  # 生成窗口
+# screen = pygame.display.set_mode(size, pygame.FULLSCREEN)  # 生成窗口
+screen = pygame.display.set_mode(size, pygame.RESIZABLE)  # 生成窗口
 speed = [1, 1]  # 设置速度列表
 BLACK = 0, 0, 0  # RGB色域黑色
 pygame.display.set_caption("pygame游戏之旅")  # 设置窗口名称
 ball = pygame.image.load("PYG02-ball.gif")  # 导入图片，表示为surface对象
+icon = pygame.image.load("ningmeng.png")
+pygame.display.set_icon(icon)
 ballrect = ball.get_rect()  # 返回覆盖图像的矩形Rect对象
-fps = 200
+fps = 300
 fclock = pygame.time.Clock()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-    ballrect = ballrect.move(speed[0], speed[1])  # 移动矩形
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                speed[0] = speed[0] if speed[0] == 0 else (abs(speed[0]) - 1) * int(speed[0] / abs(speed[0]))
+            elif event.key == pygame.K_RIGHT:
+                speed[0] = speed[0] + 1 if speed[0] > 0 else speed[0] - 1
+            elif event.key == pygame.K_UP:
+                speed[1] = speed[1] + 1 if speed[1] > 0 else speed[1] - 1
+            elif event.key == pygame.K_DOWN:
+                speed[1] = speed[1] if speed[1] == 0 else (abs(speed[1]) - 1) * int(speed[1] / abs(speed[1]))
+            #     esc退出游戏
+            elif event.key == pygame.K_ESCAPE:
+                sys.exit()
+        #         跟随窗体变化改变窗体大小
+        elif event.type == pygame.VIDEORESIZE:
+            size = width, height = event.size[0], event.size[1]
+            screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+
+    if pygame.display.get_active():                   # 当窗口没有被最小化时
+        ballrect = ballrect.move(speed[0], speed[1])  # 移动矩形
     # 设置反弹
     if ballrect.left < 0 or ballrect.right > width:
         speed[0] = -speed[0]
